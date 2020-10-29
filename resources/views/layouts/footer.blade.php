c07fb61dd2ee9f2d6dea0dcbba3b3ebce71c65e3
Kunal <gargkunal01@gmail.com>
2020-10-29 17:44:36 +0530

    Laravel

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="card bg-dark">
+  <div style="color:white" class="card-header">
+   <h4>Footer</h4>
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p  style="color:white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer style="color:white" class="blockquote-footer">Copy Registered</cite></footer>
+    </blockquote>
+  </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title')
+<title>Welcome Page</title>
+@endsection
+@section('content')
+
+<h1>Homepage</h1>
+
+
+@endsection
